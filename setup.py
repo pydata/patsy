a2bc39ab6c3a4c164ee1bde1b30b985a7d111aa3
Nathaniel J. Smith <njs@pobox.com>
2012-03-28 16:06:01 +0100

    Add some basic build/test infrastructure

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+DESC = """A Python library for describing statistical models and for
+building model matrices."""
+
+setup(
+    name="charlton",
+    version="0.0+dev",
+    description=DESC,
+    author="Nathaniel J. Smith",
+    author_email="njs@pobox.com",
+    license="2-clause BSD",
+    packages=["charlton"],
+    install_requires=["numpy"],
+    url="https://github.com/charlton/charlton",
+    )
