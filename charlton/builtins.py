82add68dc77241df8146da72a70fb99420970b03
Nathaniel J. Smith <njs@pobox.com>
2012-04-03 16:00:24 +0100

    ReST-ify docstrings for I() and Q().

--- charlton/builtins.py
+++ charlton/builtins.py
@@ -18,17 +18,17 @@ def I(x):
     """The identity function. Simply returns its input unchanged.
 
     Since Charlton's formula parser ignores anything inside a function call
-    syntax, this is useful to `hide' arithmetic operations from it. For
+    syntax, this is useful to 'hide' arithmetic operations from it. For
     instance::
 
       y ~ x1 + x2
 
-    has `x1` and `x2` as two separate predictors. But in::
+    has ``x1`` and ``x2`` as two separate predictors. But in::
 
       y ~ I(x1 + x2)
 
-    we instead have a single predictor, defined to be the sum of `x1` and
-    `x2`."""
+    we instead have a single predictor, defined to be the sum of ``x1`` and
+    ``x2``."""
     return x
 
 builtins["I"] = I
@@ -38,26 +38,31 @@ def test_I():
     assert I(None) is None
 
 def Q(name):
-    """A way to `quote' variable names, especially ones that do not otherwise
+    """A way to 'quote' variable names, especially ones that do not otherwise
     meet Python's variable name rules.
 
-    If `x`is a variable, `Q("x")` returns the value of `x`. (Note that `Q`
-    takes the *string* `"x"`, not the value of `x` itself.) This works even if
-    instead of `x`, we have a variable name that would not otherwise be legal
-    in Python.
+    If ``x`` is a variable, ``Q("x")`` returns the value of ``x``. (Note that
+    ``Q`` takes the *string* ``"x"``, not the value of ``x`` itself.) This
+    works even if instead of ``x``, we have a variable name that would not
+    otherwise be legal in Python.
 
     For example, if you have a column of data named `weight.in.kg`, then you
     can't write::
 
       y ~ weight.in.kg
 
-    because Python will try to find a variable named `weight`, that has an
-    attribute named `in`, that has an attribute named `kg`. (And worse yet,
-    `in` is a reserved word...) Instead, write::
+    because Python will try to find a variable named ``weight``, that has an
+    attribute named ``in``, that has an attribute named ``kg``. (And worse
+    yet, ``in`` is a reserved word, which makes this example doubly broken.)
+    Instead, write::
 
       y ~ Q("weight.in.kg")
 
-    and all will be well."""
+    and all will be well. Note that ``Q`` is an ordinary Python function,
+    which means that you can use it in more complex expressions. For example,
+    this is a legal formula::
+
+      y ~ np.sqrt(Q("weight.in.kg"))"""
     from charlton.eval import capture_environment
     g, l = capture_environment(1)
     if name in l:
