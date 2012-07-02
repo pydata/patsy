1667fc84b7b3838b67f489e3a6b6216995f10b46
Nathaniel J. Smith <njs@pobox.com>
2012-07-02 16:40:24 +0100

    add use_2to3 argument (but Py3 tests don't pass yet)

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup
 
 DESC = """A Python library for describing statistical models and for
 building model matrices."""
+
+# Compatibility code for handling both setuptools and distribute on Python 3,
+# as suggested here: http://packages.python.org/distribute/python3.html
+extra = {}
+if sys.version_info >= (3,):
+    extra["use_2to3"] = True
 
 setup(
     name="charlton",
@@ -14,4 +21,4 @@ setup(
     license="2-clause BSD",
     packages=["charlton"],
     url="https://github.com/charlton/charlton",
-    )
+    **extra)
