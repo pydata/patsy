9a8241f66be0d728967e12983261b20cc7db1ffb
Nathaniel J. Smith <njs@pobox.com>
2012-09-04 14:48:14 +0100

    another attempt at getting travis build working again

--- .travis-make-py24-virtualenv.sh
+++ .travis-make-py24-virtualenv.sh
@@ -6,7 +6,7 @@ set -x
 set -e
 
 # This is the last version of virtualenv to support python 2.4:
-curl https://raw.github.com/pypa/virtualenv/1.7.2/virtualenv.py
+curl -O virtualenv.py https://raw.github.com/pypa/virtualenv/1.7.2/virtualenv.py
 curl -O http://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2
 tar xjf Python-2.4.6.tar.bz2
 cd Python-2.4.6
