60a10e8b5c0954ca26bc849a2b2c4358f61b202f
Nathaniel J. Smith <njs@pobox.com>
2012-09-04 14:14:07 +0100

    Update Travis py24 script to use a working version of virtualenv
    virtualenv version 1.8+ no longer supports Python 2.4. So, when using
    Python 2.4, we fetch the last supported version and use that instead.

--- .travis-make-py24-virtualenv.sh
+++ .travis-make-py24-virtualenv.sh
@@ -5,6 +5,8 @@ VIRTENV=$1
 set -x
 set -e
 
+# This is the last version of virtualenv to support python 2.4:
+curl https://raw.github.com/pypa/virtualenv/1.7.2/virtualenv.py
 curl -O http://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2
 tar xjf Python-2.4.6.tar.bz2
 cd Python-2.4.6
@@ -15,4 +17,4 @@ EOF
 ./configure --prefix=$PWD/install
 make
 make install
-virtualenv -p install/bin/python2.4 --distribute $VIRTENV
+install/bin/python2.4 ./virtualenv.py --distribute $VIRTENV
