0316d2901f4195db06e8091c15f37d9fe4ad09de
Nathaniel J. Smith <njs@pobox.com>
2012-09-08 12:06:39 +0100

    now let's try using an older version of pip too

--- .travis-make-py24-virtualenv.sh
+++ .travis-make-py24-virtualenv.sh
@@ -5,8 +5,6 @@ VIRTENV=$1
 set -x
 set -e
 
-# This is the last version of virtualenv to support python 2.4:
-curl -O https://raw.github.com/pypa/virtualenv/1.7.2/virtualenv.py
 curl -O http://www.python.org/ftp/python/2.4.6/Python-2.4.6.tar.bz2
 tar xjf Python-2.4.6.tar.bz2
 cd Python-2.4.6
@@ -17,4 +15,11 @@ EOF
 ./configure --prefix=$PWD/install
 make
 make install
-install/bin/python2.4 ../virtualenv.py --distribute $VIRTENV
+# This is the last version of virtualenv to support python 2.4:
+curl -O https://raw.github.com/pypa/virtualenv/1.7.2/virtualenv.py
+# And this is the last version of pip to support python 2.4. If
+# there's a file matching "^pip-.*(zip|tar.gz|tar.bz2|tgz|tbz)$" in
+# the current directory then virtualenv will take that as the pip
+# source distribution to install
+curl -O http://pypi.python.org/packages/source/p/pip/pip-1.1.tar.gz
+install/bin/python2.4 ./virtualenv.py --distribute $VIRTENV
