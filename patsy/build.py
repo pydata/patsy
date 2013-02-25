pydata/patsy

commit d317c69ffd72b4c9f5f2f915d668a568fd917e46

ENH: add missing value handling
Fixes #1.

patsy/build.py
@@ -19,6 +19,7 @@ from patsy.redundancy import pick_contrasts_for_term
 from patsy.desc import ModelDesc
 from patsy.contrasts import code_contrast_matrix, Treatment
 from patsy.compat import itertools_product, OrderedDict
+from patsy.missing import NAAction
 
 if have_pandas:
     import pandas
@@ -174,8 +175,7 @@ class _CatFactorEvaluator(object):
     def eval(self, data):
         # returns either a 2d ndarray or a DataFrame
         result = self.factor.eval(self._state, data)
-        if self._postprocessor is not None:
-            result = self._postprocessor.transform(result)
+        result = self._postprocessor.transform(result)
         if not isinstance(result, Categorical):
             msg = ("when evaluating categoric factor %r, I got a "
                    "result that is not of type Categorical (but rather %s)"
@@ -189,10 +189,7 @@ class _CatFactorEvaluator(object):
                    % (self.factor.name(), self._expected_levels, result.levels))
             raise PatsyError(msg, self.factor)
         _max_allowed_dim(1, result.int_array, self.factor)
-        # For consistency, evaluators *always* return 2d arrays (though in
-        # this case it will always have only 1 column):
-        return atleast_2d_column_default(result.int_array,
-                                         preserve_pandas=True)
+        return result
 
 def test__CatFactorEvaluator():
     from nose.tools import assert_raises
@@ -292,8 +289,11 @@ class _ColumnBuilder(object):
             for factor, column_idx in zip(self._factors, column_idxs):
                 if factor in self._cat_contrasts:
                     contrast = self._cat_contrasts[factor]
-                    out[:, i] *= contrast.matrix[factor_values[factor].ravel(),
-                                                 column_idx]
+                    int_array = factor_values[factor].int_array
+                    if np.any(int_array < 0):
+                        raise PatsyError("can't build a design matrix "
+                                         "containing missing values", factor)
+                    out[:, i] *= contrast.matrix[int_array, column_idx]
                 else:
                     assert (factor_values[factor].shape[1]
                             == self._num_columns[factor])
@@ -768,10 +768,14 @@ class DesignMatrixBuilder(object):
         for evaluator, value in evaluator_to_values.iteritems():
             if evaluator in self._evaluators:
                 factor_to_values[evaluator.factor] = value
-                if num_rows is not None:
-                    assert num_rows == value.shape[0]
+                if isinstance(value, Categorical):
+                    this_num_rows = value.int_array.shape[0]
                 else:
-                    num_rows = value.shape[0]
+                    this_num_rows = value.shape[0]
+                if num_rows is not None:
+                    assert num_rows == this_num_rows
+                else:
+                    num_rows = this_num_rows
         if num_rows is None:
             # We have no dependence on the data -- e.g. an empty termlist, or
             # only an intercept term.
@@ -789,7 +793,9 @@ class DesignMatrixBuilder(object):
         assert start_column == self.total_columns
         return need_reshape, m
 
-def build_design_matrices(builders, data, return_type="matrix",
+def build_design_matrices(builders, data,
+                          NA_action="drop",
+                          return_type="matrix",
                           dtype=np.dtype(float)):
     """Construct several design matrices from :class:`DesignMatrixBuilder`
     objects.
@@ -801,6 +807,10 @@ def build_design_matrices(builders, data, return_type="matrix",
     :arg builders: A list of :class:`DesignMatrixBuilders` specifying the
       design matrices to be built.
     :arg data: A dict-like object which will be used to look up data.
+    :arg NA_action: What to do with rows that contain missing values. Either
+      ``"drop"``, ``"raise"``, or an :class:`NAAction` object. See
+      :class:`NAAction` for details on what values count as 'missing' (and how
+      to alter this).
     :arg return_type: Either ``"matrix"`` or ``"dataframe"``. See below.
     :arg dtype: The dtype of the returned matrix. Useful if you want to use
       single-precision or extended-precision.
@@ -820,12 +830,15 @@ def build_design_matrices(builders, data, return_type="matrix",
     incrementally processing a large data set, simply call this function for
     each chunk.
     """
+    if isinstance(NA_action, basestring):
+        NA_action = NAAction(NA_action)
     if return_type == "dataframe" and not have_pandas:
         raise PatsyError("pandas.DataFrame was requested, but pandas "
                             "is not installed")
     if return_type not in ("matrix", "dataframe"):
         raise PatsyError("unrecognized output type %r, should be "
                             "'matrix' or 'dataframe'" % (return_type,))
+    # Evaluate factors
     evaluator_to_values = {}
     num_rows = None
     pandas_index = None
@@ -836,30 +849,48 @@ def build_design_matrices(builders, data, return_type="matrix",
         for evaluator in builder._evaluators:
             if evaluator not in evaluator_to_values:
                 value = evaluator.eval(data)
-                assert value.ndim == 2
-                if num_rows is None:
-                    num_rows = value.shape[0]
+                if isinstance(value, Categorical):
+                    unboxed = value.int_array
                 else:
-                    if num_rows != value.shape[0]:
+                    unboxed = value
+                # unboxed may now be a Series, DataFrame, or ndarray
+                if num_rows is None:
+                    num_rows = unboxed.shape[0]
+                else:
+                    if num_rows != unboxed.shape[0]:
                         msg = ("Row mismatch: factor %s had %s rows, when "
                                "previous factors had %s rows"
-                               % (evaluator.factor.name(), value.shape[0],
+                               % (evaluator.factor.name(), unboxed.shape[0],
                                   num_rows))
                         raise PatsyError(msg, evaluator.factor)
                 if (have_pandas
-                    and isinstance(value, (pandas.Series, pandas.DataFrame))):
+                    and isinstance(unboxed, (pandas.Series, pandas.DataFrame))):
                     if pandas_index is None:
-                        pandas_index = value.index
+                        pandas_index = unboxed.index
                     else:
-                        if not pandas_index.equals(value.index):
+                        if not pandas_index.equals(unboxed.index):
                             msg = ("Index mismatch: pandas objects must "
                                    "have aligned indexes")
                             raise PatsyError(msg, evaluator.factor)
                 # Strategy: we work with raw ndarrays for doing the actual
                 # combining; DesignMatrixBuilder objects never sees pandas
                 # objects. Then at the end, if a DataFrame was requested, we
-                # convert.
-                evaluator_to_values[evaluator] = np.asarray(value)
+                # convert. So every entry in this dict is either a
+                # Categorical object, or a 2-d array of values.
+                if not isinstance(value, Categorical):
+                    value = np.asarray(value)
+                evaluator_to_values[evaluator] = value
+    # Handle NAs
+    if pandas_index is None and num_rows is not None:
+        pandas_index = np.arange(num_rows)
+    factor_values = evaluator_to_values.values()
+    origins = [evaluator.factor.origin for evaluator in evaluator_to_values]
+    new_index, new_factor_values = NA_action.handle_NA(pandas_index,
+                                                       factor_values,
+                                                       origins)
+    pandas_index = new_index
+    evaluator_to_values = dict(zip(evaluator_to_values, new_factor_values))
+    # Build factor values into matrices
     results = []
     for builder in builders:
         results.append(builder._build(evaluator_to_values, dtype))

patsy/categorical.py
@@ -11,7 +11,8 @@ from patsy.state import stateful_transform
 from patsy.util import (SortAnythingKey,
                         have_pandas, have_pandas_categorical,
                         asarray_or_pandas,
-                        pandas_friendly_reshape)
+                        pandas_friendly_reshape,
+                        safe_scalar_isnan)
 
 if have_pandas:
     import pandas
@@ -26,6 +27,10 @@ class Categorical(object):
     You should not normally need to use this class directly; it's mostly used
     as a way for :func:`C` to pass information back to the formula evaluation
     machinery.
+
+    The special integer -1 is used to indicate a missing value. (This is
+    compatible with how :class:`pandas.Categorical` represents missing
+    values.)
     """
     def __init__(self, int_array, levels, contrast=None):
         self.int_array = asarray_or_pandas(int_array, dtype=int)
@@ -42,6 +47,8 @@ class Categorical(object):
 
     @classmethod
     def from_pandas_categorical(cls, pandas_categorical):
+        """Create a Categorical object given a :class:`pandas.Categorical`
+        object."""
         return Categorical(pandas_categorical.labels,
                            pandas_categorical.levels)
 
@@ -51,13 +58,22 @@ class Categorical(object):
 
         Create a Categorical object given a sequence of data. Levels will be
         auto-detected if not given.
+
+        As far as this function is concerned, 'None' and 'NaN' values are not
+        possible levels; they will be treated as indicating missing values.
         """
+        def missing_level(level):
+            return level is None or safe_scalar_isnan(level)
         if levels is None:
             try:
                 levels = list(set(sequence))
             except TypeError:
                 raise PatsyError("Error converting data to categorical: "
                                     "all items must be hashable")
+            # Filter out any missing values. (Let's do this before sorting,
+            # just to avoid any weirdness that might arise when trying to sort
+            # NaNs...)
+            levels = [level for level in levels if not missing_level(level)]
             levels.sort(key=SortAnythingKey)
         level_to_int = {}
         for i, level in enumerate(levels):
@@ -70,7 +86,10 @@ class Categorical(object):
         int_array = np.empty(len(sequence), dtype=int)
         for i, entry in enumerate(sequence):
             try:
-                int_array[i] = level_to_int[entry]
+                if missing_level(entry):
+                    int_array[i] = -1
+                else:
+                    int_array[i] = level_to_int[entry]
             except KeyError:
                 sorted_levels = sorted(level_to_int)
                 SHOW_LEVELS = 4
@@ -162,6 +181,31 @@ def test_Categorical():
                   Categorical.from_sequence, ["a", "b", {}])
     assert_raises(PatsyError,
                   Categorical.from_sequence, ["a", "b"], levels=["a", "b", {}])
+
+def test_Categorical_missing():
+    seqs = [["a", "c", None, np.nan, "b"],
+            np.asarray("a", "c", None, np.nan, "b", dtype=object),
+            [("hi", 1), ("hi", 2), None, np.nan, ("bye", 1)],
+            ]
+    if have_pandas:
+        seqs.append(pandas.Series(["a", "c", None, np.nan, "b"]))
+    for seq in seqs:
+        c = Categorical.from_sequence(seq)
+        assert len(c.levels) == 3
+        assert np.array_equal(c.int_array, [0, 2, -1, -1, 1])
+
+    c = Categorical.from_sequence(["a", "c", None, np.nan, "b"],
+                                  levels=["c", "a", "b"])
+    assert c.levels == ("c", "a", "b")
+    assert np.array_equal(c.int_array, [1, 0, -1, -1, 2])
+
+    if have_pandas_categorical:
+        # Make sure that from_pandas_categorical works too
+        pc = pandas.Categorical(["a", "c", None, np.nan, "b"])
+        assert np.array_equal(safe_isnan(pc),
+                              [False, False, True, True, False])
+        c = Categorical.from_pandas_categorical(pc)
+        assert np.array_equal(c.int_array, [0, 2, -1, -1, 1])
 
 # contrast= can be:
 #   -- a ContrastMatrix

commit e6a117f17a6b019791384c2420426e57bd391d9d

Fix all the test failures introduced by d317c69ffd72b
(Forgot the --all-modules switch when testing before committing, doh.)

patsy/build.py
@@ -195,11 +195,14 @@ def test__CatFactorEvaluator():
     from nose.tools import assert_raises
     from patsy.categorical import Categorical
     f = _MockFactor()
-    cf1 = _CatFactorEvaluator(f, {}, None, ["a", "b"])
+    ct = CategoricalTransform()
+    ct.memorize_chunk(Categorical([0, 1], levels=("a", "b")))
+    ct.memorize_finish()
+    cf1 = _CatFactorEvaluator(f, {}, ct, ["a", "b"])
     assert cf1.factor is f
     cat1 = cf1.eval({"mock": Categorical.from_sequence(["b", "a", "b"])})
-    assert cat1.shape == (3, 1)
-    assert np.all(cat1 == [[1], [0], [1]])
+    assert cat1.int_array.shape == (3,)
+    assert np.all(cat1.int_array == [1, 0, 1])
     assert_raises(PatsyError, cf1.eval, {"mock": ["c"]})
     assert_raises(PatsyError, cf1.eval,
                   {"mock": Categorical.from_sequence(["a", "c"])})
@@ -214,20 +217,20 @@ def test__CatFactorEvaluator():
     btc = _BoolToCat(_MockFactor())
     cf2 = _CatFactorEvaluator(_MockFactor(), {}, btc, [False, True])
     cat2 = cf2.eval({"mock": [True, False, False, True]})
-    assert cat2.shape == (4, 1)
-    assert np.all(cat2 == [[1], [0], [0], [1]])
+    assert cat2.int_array.shape == (4,)
+    assert np.all(cat2.int_array == [1, 0, 0, 1])
 
     if have_pandas:
         s = pandas.Series(["b", "a"], index=[10, 20])
         cat_s = cf1.eval({"mock": Categorical.from_sequence(s)})
-        assert isinstance(cat_s, pandas.DataFrame)
-        assert np.array_equal(cat_s, [[1], [0]])
-        assert np.array_equal(cat_s.index, [10, 20])
+        assert isinstance(cat_s.int_array, pandas.Series)
+        assert np.array_equal(cat_s.int_array, [1, 0])
+        assert np.array_equal(cat_s.int_array.index, [10, 20])
         sbool = pandas.Series([True, False], index=[11, 21])
         cat_sbool = cf2.eval({"mock": sbool})
-        assert isinstance(cat_sbool, pandas.DataFrame)
-        assert np.array_equal(cat_sbool, [[1], [0]])
-        assert np.array_equal(cat_sbool.index, [11, 21])
+        assert isinstance(cat_sbool.int_array, pandas.Series)
+        assert np.array_equal(cat_sbool.int_array, [1, 0])
+        assert np.array_equal(cat_sbool.int_array.index, [11, 21])
 
 def _column_combinations(columns_per_factor):
     # For consistency with R, the left-most item iterates fastest:
@@ -312,7 +315,7 @@ def test__ColumnBuilder():
     mat = np.empty((3, 2))
     assert cb.column_names() == ["f1:f2[c1]:f3", "f1:f2[c2]:f3"]
     cb.build({f1: atleast_2d_column_default([1, 2, 3]),
-              f2: atleast_2d_column_default([0, 0, 1]),
+              f2: Categorical([0, 0, 1], levels=("c1", "c2")),
               f3: atleast_2d_column_default([7.5, 2, -12])},
              mat)
     assert np.allclose(mat, [[0, 0.5 * 1 * 7.5],
@@ -321,7 +324,7 @@ def test__ColumnBuilder():
     cb2 = _ColumnBuilder([f1, f2, f3], {f1: 2, f3: 1}, {f2: contrast})
     mat2 = np.empty((3, 4))
     cb2.build({f1: atleast_2d_column_default([[1, 2], [3, 4], [5, 6]]),
-               f2: atleast_2d_column_default([0, 0, 1]),
+               f2: Categorical([0, 0, 1], levels=("c1", "c2")),
                f3: atleast_2d_column_default([7.5, 2, -12])},
               mat2)
     assert cb2.column_names() == ["f1[0]:f2[c1]:f3",

patsy/categorical.py
@@ -184,8 +184,8 @@ def test_Categorical():
 
 def test_Categorical_missing():
     seqs = [["a", "c", None, np.nan, "b"],
-            np.asarray("a", "c", None, np.nan, "b", dtype=object),
-            [("hi", 1), ("hi", 2), None, np.nan, ("bye", 1)],
+            np.asarray(["a", "c", None, np.nan, "b"], dtype=object),
+            [("hi", 1), ("zzz", 1), None, np.nan, ("hi", 2)],
             ]
     if have_pandas:
         seqs.append(pandas.Series(["a", "c", None, np.nan, "b"]))
@@ -201,7 +201,8 @@ def test_Categorical_missing():
 
     if have_pandas_categorical:
         # Make sure that from_pandas_categorical works too
-        pc = pandas.Categorical(["a", "c", None, np.nan, "b"])
+        pc = pandas.Categorical.from_array(["a", "c", None, np.nan, "b"])
+        from patsy.util import safe_isnan
         assert np.array_equal(safe_isnan(pc),
                               [False, False, True, True, False])
         c = Categorical.from_pandas_categorical(pc)

patsy/missing.py
@@ -197,7 +197,7 @@ def test_NAAction_basic():
                                                 [None])
     assert np.array_equal(index, [1, 2])
     assert np.array_equal(factor_values[0], [0.0, 1.0])
-    assert np.array_equal(factor_values[1], ["b", "c"])
+    assert np.all(factor_values[1] == ["b", "c"])
 
 def test_NAAction_NA_types():
     for NA_types in [[], ["NaN"]]:
@@ -241,7 +241,7 @@ def test_NAAction_drop():
                    [np.nan, 5.0],
                    [6.0, 7.0],
                    [8.0, np.nan]])],
-      np.asarray([3, 1, 0]),
+      np.asarray([4, 3, 1]),
       [np.asarray([1, 2, 4]),
        np.asarray([[1.0, 2.0], [3.0, 4.0], [6.0, 7.0]])])
     