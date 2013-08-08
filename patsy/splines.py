610d7a137b3807d358f0a8cf8de34e52d07f1433
Nathaniel J. Smith <njs@pobox.com>
2013-08-08 22:15:34 +0100

    checkpoint -- wrote a bunch of code and tests, haven't tried running them yet

--- patsy/splines.py
+++ patsy/splines.py
@@ -5,9 +5,9 @@
 # R-compatible spline basis functions
 
 import numpy as np
+from scipy.interpolate import splev
 
-# make sure we correctly handle degree 0 (piecewise constant) 'spline'
-# functions.
+from patsy.state import stateful_transform
 
 def _eval_bspline_basis(x, knots, degree):
     # 'knots' are assumed to be already pre-processed. E.g. usually you
@@ -23,6 +23,14 @@ def _eval_bspline_basis(x, knots, degree):
         x = x[:, 0]
     if x.ndim > 1:
         raise ValueError("can't evaluate spline on multidim array")
+    # XX FIXME: when points fall outside of the boundaries, splev and R seem
+    # to handle them differently. I don't know why yet. So until we understand
+    # this and decide what to do with it, I'm going to play it safe and
+    # disallow such points.
+    if np.min(x) < np.min(knots) or np.max(x) > np.max(knots):
+        raise NotImplementedError("some data points fall outside the "
+                                  "outermost knots, and I'm not sure how "
+                                  "to handle them. (Patches accepted!)")
     # Thanks to Charles Harris for explaining splev. It's not well
     # documented, but basically it computes an arbitrary b-spline basis
     # given knots and degree on some specificed points (or derivatives
@@ -35,7 +43,6 @@ def _eval_bspline_basis(x, knots, degree):
     # function for coefficients that are zero).
     # Note: the order of a spline is the same as its degree + 1.
     # Note: there are (len(knots) - order) basis functions.
-    from scipy.interpolate import splev
     n_bases = len(knots) - (degree + 1)
     basis = np.empty((x.shape[0], n_bases), dtype=float)
     for i in xrange(n_bases):
@@ -44,37 +51,205 @@ def _eval_bspline_basis(x, knots, degree):
         basis[:, i] = splev(x, (knots, coefs, degree))
     return basis
 
+def _R_compat_quantile(x, probs):
+    return np.percentile(x, 100 * np.asarray(probs))
+
+def test__R_compat_quantile():
+    def t(x, prob, expected):
+        assert np.allclose(_R_compat_quantile(x, prob), expected)
+    t([10, 20], 0.5, 15)
+    t([10, 20], 0.3, 13)
+    t([10, 20], [0.3, 0.7], [13, 17])
+    t(range(10), [0.3, 0.7], [2.7, 6.3])
+
 class BS(object):
+    """bs(x, df=None, knots=None, degree=3, include_intercept=False, lower_bound=None, upper_bound=None)
+
+    Generates B-spline bases useful for spline regression. The usual usage is
+    something like::
+
+      y ~ 1 + bs(x, 3)
+
+    to fit `y` as a 3 degree-of-freedom smooth function of `x`.
+
+    :arg df: The number of degrees of freedom to use for this spline. The
+      return value will have this many columns. You must specify at least one
+      of `df` and `knots`.
+    :arg knots: The interior knots to use for the spline. If unspecified, then
+      equally spaced quantiles of the input data are used. You must specify at
+      least one of `df` and `knots`.
+    :arg degree: The degree of the spline to use.
+    :arg include_intercept: If `True`, then the resulting
+      spline basis will span the intercept term (i.e., the constant
+      function). If `False` (the default) then this will not be the case,
+      which is useful for avoiding overspecification in models that include
+      multiple spline terms and/or an intercept term.
+    :arg lower_bound: The lower exterior knot location.
+    :arg upper_bound: The upper exterior knot location.
+
+    A degree 0 spline is piecewise constant with breakpoints at each knot, and
+    the default knot positions are quantiles of the input. So if you find
+    yourself in the situation of wanting to quantize a continuous variable
+    into equal-sized bins with a constant effect across each bin, you can use
+    `bs(x, num_bins, degree=0)`.
+
+    .. warning:: I'm not sure on what the proper handling of points outside
+      the lower/upper bounds is, so for now attempting to evaluate a spline
+      basis at such points produces an error. Patches gratefully accepted.
+    """
     def __init__(self):
-        self._xs = []
-        self._lower_bound = np.inf
-        self._upper_bound = -np.inf
+        self._tmp = {}
+        self._degree = None
+        self._all_knots = None
 
     def memorize_chunk(self, x, df=None, knots=None, degree=3,
-                       span_intercept=False,
+                       include_intercept=False,
                        lower_bound=None, upper_bound=None):
+        args = {"df": df,
+                "knots": knots,
+                "degree": degree,
+                "include_intercept": include_intercept,
+                "lower_bound": lower_bound,
+                "upper_bound": upper_bound,
+                }
+        self._tmp["args"] = args
+        # XX: check whether we need x values before saving them
         x = np.atleast_1d(x)
         if x.ndim > 1:
-            raise ValueError, "input to 'bs' must be 1-d vector"
-        self._need_knots = (knots is None)
-        if self._need_knots:
-            if df is None:
-                raise ValueError, "must specify either df or knots"
-            self._knots_wanted = df - (degree + 1)
-            if span_intercept:
-                self._knots_wanted -= 1
-            # There's no better way to compute exact quantiles than 
-            self._xs.append(x)
-        if lower_bound is None:
-            self._lower_bound = min(self._lower_bound, np.min(x))
-        if upper_bound is None:
-            self._upper_bound = max(self._upper_bound, np.max(x))
+            raise ValueError("input to 'bs' must be 1-d vector")
+        # There's no better way to compute exact quantiles than memorizing
+        # all data.
+        self._tmp.setdefault("xs", []).append(x)
 
     def memorize_finish(self):
-        if not self._need_memorize:
-            return
         # These are guaranteed to all be 1d vectors by the code above
-        x = np.concatenate(self._xs)
-        # Just go ahead and calculate these unconditionally, 
-        self._lower_bound = np.min(x)
-        self._upper_bound = np.max(x)
+        tmp = self._tmp
+        args = tmp["args"]
+        del self._tmp
+        x = np.concatenate(tmp["xs"])
+        if args["df"] is None and args["knots"] is None:
+            raise ValueError("must specify either df or knots")
+        order = self._arg_degree + 1
+        if args["df"] is not None:
+            n_inner_knots = args["df"] - order
+            if not args["include_intercept"]:
+                n_inner_knots += 1
+            if n_inner_knots < 0:
+                raise ValueError("df=%r is too small for degree=%r and "
+                                 "include_intercept=%r; must be >= %s"
+                                 % (args["df"], args["degree"],
+                                    args["include_intercept"],
+                                    # We know that n_inner_knots is negative;
+                                    # if df were that much larger, it would
+                                    # have been zero, and things would work.
+                                    args["df"] - n_inner_knots))
+            if args["knots"] is not None:
+                if len(args["knots"]) != n_inner_knots:
+                    raise ValueError("df=%s implies %s knots, but %s "
+                                     "knots were provided"
+                                     % (args["df"], n_inner_knots,
+                                        len(args["knots"])))
+                else:
+                    inner_knots = args["knots"]
+            else:
+                # Need to compute inner knots
+                knot_quantiles = np.linspace(0, 1, n_inner_knots + 2)[1:-1]
+                inner_knots = _R_compat_quantile(x, knot_quantiles)
+        if args["lower_bound"] is not None:
+            lower_bound = args["lower_bound"]
+        else:
+            lower_bound = np.min(x)
+        if args["upper_bound"] is not None:
+            upper_bound = args["upper_bound"]
+        else:
+            upper_bound = np.max(x)
+        all_knots = np.concatenate(([lower_bound, upper_bound] * order,
+                                    inner_knots))
+        all_knots.sort()
+
+        self._degree = degree
+        self._all_knots = all_knots
+
+    def transform(self, x, df=None, knots=None, degree=3,
+                  include_intercept=False,
+                  lower_bound=None, upper_bound=None):
+        basis = _eval_bspline_basis(x, self._all_knots, self._degree)
+        if not include_intercept:
+            basis = basis[:, 1:]
+        return basis
+
+bs = stateful_transform(BS)
+
+def test_bs_compat():
+    from patsy.test_state import stateful_test
+    from patsy.test_splines_bs_data import (R_bs_test_x,
+                                            R_bs_test_data,
+                                            R_bs_num_tests)
+    lines = R_bs_test_data.split("\n")
+    tests_ran = 0
+    start_idx = lines.index("--BEGIN TEST CASE--")
+    while True:
+        if not lines[start_idx] == "--BEGIN TEST CASE--":
+            break
+        start_idx += 1
+        stop_idx = lines.index("--END TEST CASE--", start_idx)
+        block = lines[start_idx:stop_idx]
+        test_data = {}
+        for line in block:
+            key, value = line.split("=")
+            test_data[key] = value
+        # Translate the R output into Python calling conventions
+        kwargs = {
+            "degree": int(test_data["degree"]),
+            "df": eval(test_data["df"]),
+            "knots": eval(test_data["knots"]),
+            }
+        # Special case: R doesn't distinguish between [] and None. So the case
+        # where df=NULL, knots=NULL is actually interpreted like df=None,
+        # knots=[].
+        if kwargs["df"] is None and kwargs["knots"] is None:
+            kwargs["knots"] = []
+        if data["Boundary.knots"] != "None":
+            lower, upper = eval(test_data["Boundary.knots"])
+            kwargs["lower_bound"] = lower
+            kwargs["upper_bound"] = upper
+        kwargs["include_intercept"] = (data["intercept"] == "TRUE")
+        # Do the actual test
+        stateful_test(BS, R_bs_test_x, **kwargs)
+        tests_ran += 1
+        # Set up for the next one
+        start_idx = stop_idx + 1
+    assert tests_ran == R_bs_num_tests
+
+# Test x vector is (1.5 ** arange(20))
+# Try with all combinations of:
+#   degree 1, 3, 5
+#     specified knots, df = 3, 5, 10
+#   include_intercept True/False
+#   upper_bound = (1.5 ** 19), (1.5 ** 20)
+#   lower_bound = (1.5 ** 0) == 1, 0
+# check:
+#   df = result.shape[1]
+#   matches R
+# combined or broken input
+#
+# special case in reading this: df=None and knots=None should translate to
+# df=None, knots=[]
+#
+# error checks:
+#   out of bounds
+#   df/knots mismatch (with and without intercept)
+#   negative degree
+#   upper_bound < lower_bound
+#   knots > upper_bound
+#   knots < lower_bound
+#
+# one off checks:
+#   degree 0 (R doesn't support this)
+
+# ns:
+# - degree is always 3
+# - different number of interior knots given df (b/c fewer dof used at edges I
+#   guess)
+# - boundary knots always repeated exactly 4 times (same as bs with degree=3)
+# - complications at the end to
--- patsy/state.py
+++ patsy/state.py
@@ -26,8 +26,8 @@
 
 import numpy as np
 from patsy.util import (atleast_2d_column_default,
-                           asarray_or_pandas, pandas_friendly_reshape,
-                           wide_dtype_for)
+                        asarray_or_pandas, pandas_friendly_reshape,
+                        wide_dtype_for)
 from patsy.compat import wraps
 
 # These are made available in the patsy.* namespace
@@ -51,7 +51,7 @@ def stateful_transform(class_):
 # class NonIncrementalStatefulTransform(object):
 #     def __init__(self):
 #         self._data = []
-#    
+#
 #     def memorize_chunk(self, input_data, *args, **kwargs):
 #         self._data.append(input_data)
 #         self._args = _args
@@ -74,94 +74,7 @@ def stateful_transform(class_):
 #
 # class QuantileEstimatingTransform(NonIncrementalStatefulTransform):
 #     def memorize_all(self, input_data, *args, **kwargs):
-        
-def _test_stateful(cls, input, output, *args, **kwargs):
-    input = np.asarray(input)
-    output = np.asarray(output)
-    test_cases = [
-        # List input, one chunk
-        ([input], output),
-        # Scalar input, many chunks
-        (input, output),
-        # List input, many chunks:
-        ([[n] for n in input], output),
-        # 0-d array input, many chunks:
-        ([np.array(n) for n in input], output),
-        # 1-d array input, one chunk:
-        ([np.array(input)], output),
-        # 1-d array input, many chunks:
-        ([np.array([n]) for n in input], output),
-        # 2-d array input, one chunk:
-        ([np.column_stack((input, input[::-1]))],
-         np.column_stack((output, output[::-1]))),
-        # 2-d array input, many chunks:
-        ([np.array([[input[i], input[-i-1]]]) for i in xrange(len(input))],
-         np.column_stack((output, output[::-1]))),
-        ]
-    from patsy.util import have_pandas
-    if have_pandas:
-        import pandas
-        pandas_type = (pandas.Series, pandas.DataFrame)
-        pandas_index = np.linspace(0, 1, num=len(input))
-        output_series = pandas.Series(output, index=pandas_index)
-        input_2d = np.column_stack((input, input[::-1]))
-        output_2d = np.column_stack((output, output[::-1]))
-        output_dataframe = pandas.DataFrame(output_2d, index=pandas_index)
-        test_cases += [
-            # Series input, one chunk
-            ([pandas.Series(input, index=pandas_index)], output_series),
-            # Series input, many chunks
-            ([pandas.Series([x], index=[idx])
-              for (x, idx) in zip(input, pandas_index)],
-             output_series),
-            # DataFrame input, one chunk
-            ([pandas.DataFrame(input_2d, index=pandas_index)], output_dataframe),
-            # DataFrame input, many chunks
-            ([pandas.DataFrame([input_2d[i, :]], index=[pandas_index[i]])
-              for i in xrange(len(input))],
-             output_dataframe),
-            ]
-    for input_obj, output_obj in test_cases:
-        print input_obj
-        t = cls()
-        for input_chunk in input_obj:
-            t.memorize_chunk(input_chunk, *args, **kwargs)
-        t.memorize_finish()
-        all_outputs = []
-        for input_chunk in input_obj:
-            output_chunk = t.transform(input_chunk, *args, **kwargs)
-            assert output_chunk.ndim == np.asarray(input_chunk).ndim
-            all_outputs.append(output_chunk)
-        if have_pandas and isinstance(all_outputs[0], pandas_type):
-            all_output1 = pandas.concat(all_outputs)
-            assert np.array_equal(all_output1.index, pandas_index)
-        elif all_outputs[0].ndim == 0:
-            all_output1 = np.array(all_outputs)
-        elif all_outputs[0].ndim == 1:
-            all_output1 = np.concatenate(all_outputs)
-        else:
-            all_output1 = np.row_stack(all_outputs)
-        assert all_output1.shape[0] == len(input)
-        # output_obj_reshaped = np.asarray(output_obj).reshape(all_output1.shape)
-        # assert np.allclose(all_output1, output_obj_reshaped)
-        assert np.allclose(all_output1, output_obj)
-        if np.asarray(input_obj[0]).ndim == 0:
-            all_input = np.array(input_obj)
-        elif have_pandas and isinstance(input_obj[0], pandas_type):
-            # handles both Series and DataFrames
-            all_input = pandas.concat(input_obj)
-        elif np.asarray(input_obj[0]).ndim == 1:
-            # Don't use row_stack, because that would turn this into a 1xn
-            # matrix:
-            all_input = np.concatenate(input_obj)
-        else:
-            all_input = np.row_stack(input_obj)
-        all_output2 = t.transform(all_input, *args, **kwargs)
-        if have_pandas and isinstance(input_obj[0], pandas_type):
-            assert np.array_equal(all_output2.index, pandas_index)
-        assert all_output2.ndim == all_input.ndim
-        assert np.allclose(all_output2, output_obj)
-    
+
 class Center(object):
     """center(x)
 
@@ -203,40 +116,6 @@ class Center(object):
         return pandas_friendly_reshape(centered, x.shape)
 
 center = stateful_transform(Center)
-
-def test_Center():
-    _test_stateful(Center, [1, 2, 3], [-1, 0, 1])
-    _test_stateful(Center, [1, 2, 1, 2], [-0.5, 0.5, -0.5, 0.5])
-    _test_stateful(Center,
-                   [1.3, -10.1, 7.0, 12.0],
-                   [-1.25, -12.65, 4.45, 9.45])
-
-
-def test_stateful_transform_wrapper():
-    assert np.allclose(center([1, 2, 3]), [-1, 0, 1])
-    assert np.allclose(center([1, 2, 1, 2]), [-0.5, 0.5, -0.5, 0.5])
-    assert center([1.0, 2.0, 3.0]).dtype == np.dtype(float)
-    assert (center(np.array([1.0, 2.0, 3.0], dtype=np.float32)).dtype
-            == np.dtype(np.float32))
-    assert center([1, 2, 3]).dtype == np.dtype(float)
-
-    from patsy.util import have_pandas
-    if have_pandas:
-        import pandas
-        s = pandas.Series([1, 2, 3], index=["a", "b", "c"])
-        df = pandas.DataFrame([[1, 2], [2, 4], [3, 6]],
-                              columns=["x1", "x2"],
-                              index=[10, 20, 30])
-        s_c = center(s)
-        assert isinstance(s_c, pandas.Series)
-        assert np.array_equal(s_c.index, ["a", "b", "c"])
-        assert np.allclose(s_c, [-1, 0, 1])
-        df_c = center(df)
-        assert isinstance(df_c, pandas.DataFrame)
-        assert np.array_equal(df_c.index, [10, 20, 30])
-        assert np.array_equal(df_c.columns, ["x1", "x2"])
-        assert np.allclose(df_c, [[-1, -2], [0, 0], [1, 2]])
-        
 
 # See:
 #   http://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#On-line_algorithm
@@ -295,45 +174,3 @@ class Standardize(object):
 standardize = stateful_transform(Standardize)
 # R compatibility:
 scale = standardize
-
-def test_Standardize():
-    _test_stateful(Standardize, [1, -1], [1, -1])
-    _test_stateful(Standardize, [12, 10], [1, -1])
-    _test_stateful(Standardize,
-                   [12, 11, 10],
-                   [np.sqrt(3./2), 0, -np.sqrt(3./2)])
-
-    _test_stateful(Standardize,
-                   [12.0, 11.0, 10.0],
-                   [np.sqrt(3./2), 0, -np.sqrt(3./2)])
-
-    # XX: see the comment in Standardize.transform about why this doesn't
-    # work:
-    # _test_stateful(Standardize,
-    #                [12.0+0j, 11.0+0j, 10.0],
-    #                [np.sqrt(3./2)+0j, 0, -np.sqrt(3./2)])
-
-    _test_stateful(Standardize, [1, -1], [np.sqrt(2)/2, -np.sqrt(2)/2],
-                   ddof=1)
-
-    _test_stateful(Standardize,
-                   range(20),
-                   list((np.arange(20) - 9.5) / 5.7662812973353983),
-                   ddof=0)
-    _test_stateful(Standardize,
-                   range(20),
-                   list((np.arange(20) - 9.5) / 5.9160797830996161),
-                   ddof=1)
-    _test_stateful(Standardize,
-                   range(20),
-                   list((np.arange(20) - 9.5)),
-                   rescale=False, ddof=1)
-    _test_stateful(Standardize,
-                   range(20),
-                   list(np.arange(20) / 5.9160797830996161),
-                   center=False, ddof=1)
-    _test_stateful(Standardize,
-                   range(20),
-                   range(20),
-                   center=False, rescale=False, ddof=1)
-    
