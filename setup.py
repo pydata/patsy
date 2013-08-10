535b99829318aa876e4964848e5c1601fdf5ae82
Nathaniel J. Smith <njs@pobox.com>
2013-08-10 17:31:16 +0100

    [release] bump release numbers to 0.2.0+dev

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.version_info >= (3,):
 
 setup(
     name="patsy",
-    version="0.2.0",
+    version="0.2.0+dev",
     description=DESC,
     long_description=LONG_DESC,
     author="Nathaniel J. Smith",
