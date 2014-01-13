18f6c5bed8b7698ca7e6022b0690e13f5d044e75
Nathaniel J. Smith <njs@pobox.com>
2014-01-13 12:21:11 +0000

    remove testing code now that py3.3-on-travis 2to3 bug is fixed

--- patsy/contrasts.py
+++ patsy/contrasts.py
@@ -78,14 +78,6 @@ def test__obj_to_readable_str():
     def t(obj, expected):
         got = _obj_to_readable_str(obj)
         assert type(got) is str
-        print "obj", repr(obj), obj
-        print "expected", repr(expected), expected, [ord(c) for c in expected]
-        print "got", repr(got), got, [ord(c) for c in got]
-        if sys.version_info >= (3,) and isinstance(obj, bytes):
-            try:
-                print "obj.decode(utf-8)", repr(obj.decode("utf-8")), obj.decode("utf-8"), [ord(c) for c in obj.decode("utf-8")]
-            except UnicodeDecodeError:
-                print "obj.decode(utf-8) -> errored"
         assert got == expected
     t(1, "1")
     t(1.0, "1.0")
