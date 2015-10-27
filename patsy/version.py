34082b6f1b0f4f91b1f0ed7f81daf3468106915e
Nathaniel J. Smith <njs@pobox.com>
2015-10-26 22:39:05 -0700

    Fix version numbers for dev versions
    Apparently I was embarrassingly confused about what PEP 440 said. I
    cannot reconstruct what I was thinking. Oh well! Fixed.

--- patsy/version.py
+++ patsy/version.py
@@ -6,18 +6,15 @@
 # places -- it is imported by patsy/__init__.py, execfile'd by setup.py, etc.
 
 # We use a simple scheme:
-#   1.0.0 -> 1.0.0-dev -> 1.1.0 -> 1.1.0-dev
-# where the -dev versions are never released into the wild, they're just what
+#   1.0.0 -> 1.0.0+dev -> 1.1.0 -> 1.1.0+dev
+# where the +dev versions are never released into the wild, they're just what
 # we stick into the VCS in between releases.
 #
 # This is compatible with PEP 440:
 #   http://legacy.python.org/dev/peps/pep-0440/
-# in a slightly abusive way -- PEP 440 provides no guidance on what version
-# number to use for *unreleased* versions, so we use an "integrator suffix",
-# which is intended to be used for things like Debian's locally patched
-# version, and is not allowed on public index servers. Which sounds about
-# right, actually... Crucially, PEP 440 says that "foo-bar" sorts *after*
-# "foo", which is what we want for a dev version. (Compare to "foo.dev0",
-# which sorts *before* "foo".)
+# via the use of the "local suffix" "+dev", which is disallowed on index
+# servers and causes 1.0.0+dev to sort after plain 1.0.0, which is what we
+# want. (Contrast with the special suffix 1.0.0.dev, which sorts *before*
+# 1.0.0.)
 
-__version__ = "0.4.0-dev"
+__version__ = "0.4.0+dev"
